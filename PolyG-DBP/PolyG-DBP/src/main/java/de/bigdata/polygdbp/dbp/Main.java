64ced06d096ac1ec905362f25e2191b419e025ab
Tim Niehoff <niehoff@studserv.uni-leipzig.de>
2018-07-10 16:24:47 +0200

    initial commit polyg-dbp

---
+++ PolyG-DBP/PolyG-DBP/src/main/java/de/bigdata/polygdbp/dbp/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bigdata.polygdbp;
+
+/**
+ *
+ * @author tim
+ */
+public class Main {
+
+public static void main(String[] argv){
+    System.out.println("cv1");
+}    
+}
