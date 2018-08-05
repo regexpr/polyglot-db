5f3e0a5508ef3240a50170d3c55b2e0060481765
Tim Niehoff <niehoff@studserv.uni-leipzig.de>
2018-08-05 21:07:28 +0200

    NEO4J EXAMPLES WORK NOW FOR DOCMANAGER IMPORTED DATASET

--- PolyG-DBP/PolyG-DBP/src/main/java/de/polygdbp/Neo4jExamples.java
+++ PolyG-DBP/PolyG-DBP/src/main/java/de/polygdbp/Neo4jExamples.java
@@ -23,32 +23,32 @@ import static de.polygdbp.Main.LOG;
  */
 public class Neo4jExamples {
   // Output all business names and ids a <specific user> rated with minumum of <stars>
-//  private final String q1 = "MATCH (u:user)--(r:review)--(b:business) where u._id=\"nOTl4aPC4tKHK35T3bNauQ\" and r.stars > 4 return b.name, b._id";
-  private final String q1 ="MATCH (u:User)-[:WROTE]-(r:Review)-[:REVIEWS]-(Business) WHERE u.id = 'nOTl4aPC4tKHK35T3bNauQ' AND r.stars > 4 Return Business.name, Business.id";
+private final String q1 = "MATCH (u:user)--(r:review)--(b:business) where u._id=\"nOTl4aPC4tKHK35T3bNauQ\" and r.stars > 4 return b.name, b._id";
+//  private final String q1 ="MATCH (u:User)-[:WROTE]-(r:Review)-[:REVIEWS]-(Business) WHERE u.id = 'nOTl4aPC4tKHK35T3bNauQ' AND r.stars > 4 Return Business.name, Business.id";
   
   //Output the average stars of all businesses
-  //private final String q2 = "match (b:business) return avg(b.stars)";
-  private final String q2 = "MATCH (b:Business) return avg(b.stars)";
+  private final String q2 = "match (b:business) return avg(b.stars)";
+  //private final String q2 = "MATCH (b:Business) return avg(b.stars)";
   
   //Output the average stars of all businesses that grouped by category
-  //private final String q3 = "MATCH (b:business) UNWIND b.categories AS cat RETURN cat, avg(b.stars)";
-  private final String q3 = "MATCH (b:Business)--(c:Category) return c.id, avg(b.stars)";
+  private final String q3 = "MATCH (b:business) UNWIND b.categories AS cat RETURN cat, avg(b.stars)";
+  //private final String q3 = "MATCH (b:Business)--(c:Category) return c.id, avg(b.stars)";
   
   //Output the average stars of all businesses grouped by category descending order
-  // private final String q4 = "MATCH (b:business) UNWIND b.categories AS cat RETURN cat, avg(b.stars) AS average_stars ORDER BY average_stars DESC";
-  private final String q4 = "MATCH (b:Business)--(c:Category) return c.id, avg(b.stars) AS average_stars ORDER BY average_stars DESC";
+   private final String q4 = "MATCH (b:business) UNWIND b.categories AS cat RETURN cat, avg(b.stars) AS average_stars ORDER BY average_stars DESC";
+  //private final String q4 = "MATCH (b:Business)--(c:Category) return c.id, avg(b.stars) AS average_stars ORDER BY average_stars DESC";
   
   //Output all businesses that are in the category Cannabis Tours and return the average of all stars grouped by all the categories that they are in
-  //private final String q5 = "MATCH (b:business) WHERE "Shopping" IN b.categories UNWIND b.categories AS cat RETURN cat, avg(b.stars)";
-  private final String q5 = "MATCH (c1:Category)--(Business)--(c2:Category) WHERE c1.id ='Cannabis Tours' RETURN c2.id, avg(Business.stars)";
+  private final String q5 = "MATCH (b:business) WHERE 'Shopping' IN b.categories UNWIND b.categories AS cat RETURN cat, avg(b.stars)";
+  //private final String q5 = "MATCH (c1:Category)--(Business)--(c2:Category) WHERE c1.id ='Cannabis Tours' RETURN c2.id, avg(Business.stars)";
   
   //Output all businesses that were reviewed more than 700 times
-  //private final String q6 = "MATCH (b:business) Where b.review_count>700 Return b.name, b.id";
-  private final String q6 = "MATCH (b:Business) Where b.review_count>700 Return b.name, b.id";
+  private final String q6 = "MATCH (b:business) Where b.review_count>700 Return b.name, b.id";
+  //private final String q6 = "MATCH (b:Business) Where b.review_count>700 Return b.name, b.id";
   
   // The same query as above, but this time we count the reviews in the reviews collection
-  //private final String q7 = "MATCH (b:business) Where b.review_count>700 Return b.name, b.id";
-  private final String q7 = "MATCH (b:Business)<-[r:REVIEWS]-() WITH b, count(r) as reviewCount WHERE reviewCount>700 RETURN b.id, b.name, reviewCount";
+  private final String q7 = "MATCH (b:business) Where b.review_count>700 Return b.name, b.id";
+  //private final String q7 = "MATCH (b:Business)<-[r:REVIEWS]-() WITH b, count(r) as reviewCount WHERE reviewCount>700 RETURN b.id, b.name, reviewCount";
   /**
    * Get the Query by the related shortcut.
    * @param q Shortcut for the Query. Valid Example: "q1".
